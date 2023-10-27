b94ccd0f7259e03876806d90d969ead3ec486db1
elsymerino <elsym.aguilar@gmail.com>
2023-10-26 20:21:00 -0500

    Subida de proyecto1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
@@ -14,98 +15,44 @@ class MyApp extends StatelessWidget {
         title: Text('Ventana Modal Ejemplo'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Registrarse'),
-                      content: Container(
-                        width: 300,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            TextField(
-                              decoration: InputDecoration(labelText: 'Usuario'),
-                            ),
-                            SizedBox(height: 10),
-                            TextField(
-                              decoration: InputDecoration(labelText: 'Correo'),
-                            ),
-                            SizedBox(height: 10),
-                            TextField(
-                              decoration: InputDecoration(
-                                  labelText: 'Número de Teléfono'),
-                            ),
-                            SizedBox(height: 10),
-                            TextField(
-                              decoration:
-                                  InputDecoration(labelText: 'Contraseña'),
-                              obscureText: true,
-                            ),
-                          ],
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              barrierDismissible:
+                  false, // Evita que se cierre al hacer clic fuera
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Inicio de Sesión'),
+                  content: Container(
+                    width: 300,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(labelText: 'Usuario'),
                         ),
-                      ),
-                      actions: <Widget>[
-                        TextButton(
-                          child: Text('Guardar'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
+                        SizedBox(height: 10),
+                        TextField(
+                          decoration: InputDecoration(labelText: 'Contraseña'),
+                          obscureText: true,
                         ),
                       ],
-                    );
-                  },
+                    ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('Guardar'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
                 );
               },
-              child: Text('Registrarse'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Inicio de Sesión'),
-                      content: Container(
-                        width: 300,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            TextField(
-                              decoration: InputDecoration(labelText: 'Usuario'),
-                            ),
-                            SizedBox(height: 10),
-                            TextField(
-                              decoration:
-                                  InputDecoration(labelText: 'Contraseña'),
-                              obscureText: true,
-                            ),
-                          ],
-                        ),
-                      ),
-                      actions: <Widget>[
-                        TextButton(
-                          child: Text('Guardar'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Text('Iniciar Sesión'),
-            ),
-          ],
+            );
+          },
+          child: Text('Iniciar Sesion'),
         ),
       ),
     );
